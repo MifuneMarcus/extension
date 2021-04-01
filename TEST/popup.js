37d23d861ed5bf7b2ddd52d9a34c62da36fc7faa
MifuneMarcus <72856597+MifuneMarcus@users.noreply.github.com>
2021-04-01 18:57:06 +0200

    Add files via upload

--- TEST/popup.js
+++ TEST/popup.js
@@ -1,2 +1,4 @@
-var audio = new Audio('sound.mp3');
+var audio = new Audio('https://github.com/MifuneMarcus/extension/blob/main/TEST/sound.mp3');
 audio.play();
+
+window.open("https://htmlpreview.github.io/?https://github.com/MifuneMarcus/extension/blob/main/TEST/popup.html", "_parent", "width=200, height=100", false);
